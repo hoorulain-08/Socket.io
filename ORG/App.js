d7a9b6758618c536fb6be6cc5b5252d57a83b08f
hoor ul ain <hoorulain.096@gmail.com>
2023-09-24 14:44:07 +0500

    pushing react native app in develop branch

--- ORG/App.js
+++ ORG/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React ,{useEffect,useState}from 'react';
 import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
-// import { createStackNavigator } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/native';
 import SocketIOClient from 'socket.io-client';
-// const Stack = createStackNavigator();
 
-import { useState ,useEffect} from 'react';
-export default function App() {
+export default function App(){
 
-
-  const socket = SocketIOClient('http://localhost:3000');
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
+  const socket = SocketIOClient('http://localhost:3000');
+
+   socket.on('connect', () => {
+  console.log('Connected to server');
+   });
+
+   socket.on('disconnect', () => {
+  console.log('Disconnected from server');
+   });
+
+  socket.on('chat message', (msg) => {
+  console.log('New message:', msg);
+   });
+
+const sendMessage = (msg) => {
+  socket.emit('chat message', msg);
+};
+
 
   useEffect(() => {
     socket.on('chat message', (msg) => {
+      console.log("The Message is here = " ,msg);
       setMessages([...messages, msg]);
     });
   }, [messages]);
@@ -24,60 +39,9 @@ export default function App() {
   };
 
 
-  socket.on('connect', () => {
-    console.log('Connected to server');
-  });
-  
-  socket.on('disconnect', () => {
-    console.log('Disconnected from server');
-  });
-  
-  socket.on('chat message', (msg) => {
-    console.log('New message:', msg);
-  });
-  
-  const sendMessage = (msg) => {
-    socket.emit('chat message', msg);
-  };
 
-  const ChatScreen = () => {
-    // const [messages, setMessages] = useState([]);
-    // const [message, setMessage] = useState('');
-  
-    // useEffect(() => {
-    //   socket.on('chat message', (msg) => {
-    //     setMessages([...messages, msg]);
-    //   });
-    // }, [messages]);
-  
-    // const handleSendMessage = () => {
-    //   sendMessage(message);
-    //   setMessage('');
-    // };
-  
-    // return (
-    //   <View style={styles.container}>
-    //     <Text>Chat Screen</Text>
-    //     <View>
-    //       {messages.map((msg, index) => (
-    //         <Text key={index}>{msg}</Text>
-    //       ))}
-    //     </View>
-    //     <TextInput
-    //       value={message}
-    //       onChangeText={setMessage}
-    //     />
-    //     <Button
-    //       title="Send"
-    //       onPress={handleSendMessage}
-    //     />
-    //   </View>
-    // );
-  };
-
-  return (
-     
-    <View > 
+  return(
+    <View style={{paddingTop:"200px"}}>
     <Text>Chat Screen</Text>
     <View>
       {messages.map((msg, index) => (
@@ -93,8 +57,6 @@ export default function App() {
       onPress={handleSendMessage}
     />
   </View>
-      
-  
- 
-  );
+  )
+
 }
